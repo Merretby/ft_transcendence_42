0230f8fd848d0cef285e0c088e6eee5d38f6c970
oualid <oualidhassani215@gmail.com>
2025-09-05 21:12:40 +0000

    still just the begin , begin the database with sqlite

---
+++ srcs/index.js
@@ -0,0 +1,30 @@
+import Fastify from 'fastify';
+
+ const app = Fastify(
+ {
+    logger : true 
+ }
+ );
+
+// app.get('/user', (req , reply) =>
+// {
+//     // return {
+//     //     message: "hello my server fastify "
+//     // }
+//     reply.send("hello my server fastify");
+// });
+
+app.register(require('@app/cors'), {
+   origin:true 
+} )
+
+app.register
+try
+{
+    app.listen({port: 6767});
+} 
+catch(error)
+{
+    app.log.error(error);
+    process.exit(1);
+}
